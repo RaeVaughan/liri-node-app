929c2666831c809accf066beebc5830c5d298166
RaeVaughan <rachelev.tx@gmail.com>
2017-06-20 20:00:56 -0500

    initial site files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+  consumer_key: "FKqm4ZyAbCsJAQRIeNRQoYf15",
+  consumer_secret: "FANMDnhytSiGNeHPVIy7HLLAPOfFK0rfjc9JQDBP0v4FOu1LNL",
+  access_token_key: "877273758817558528-3oHp8dEINQGRurDkElXIvEb90q9jXWw",
+  access_token_secret: "Y4CLBAy2hVdP0GrTK1IsgEkl2rLcC9BSNpcINjWaRVzsk>",
+}
